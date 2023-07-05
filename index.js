e309e848879c922bf39461a1f6be37e61f3bcda6
vladickostin <vladic.kostin@edovo.com>
2023-07-05 13:58:49 -0500

    Enable all cors

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ global.__basedir = path.resolve() + "/..";
 
 const app = express();
 
-var corsOptions = {
-  origin: "https://www.vladickostin.com/"
-};
+// var corsOptions = {
+//   origin: "https://www.vladickostin.com/"
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
